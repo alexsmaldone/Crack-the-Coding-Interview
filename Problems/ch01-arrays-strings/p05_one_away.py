2b63b37a4f403780c4c911e3da1504f7b0566c06
alexsmaldone <alexsmaldone@gmail.com>
2022-03-20 12:32:18 -0400

    problem 6 strign compression

--- Problems/ch01-arrays-strings/p05_one_away.py
+++ Problems/ch01-arrays-strings/p05_one_away.py
@@ -1,2 +1,20 @@
 # There are 3 types of edits that can be perfromed on a string -- insert a character, remove a character, or replace a character.
 # Given two strings, write function to check if they are one edit or zero edits away
+
+
+# pale, ple TRUE
+# pales, pale TRUE
+# pale, bale TRUE
+# pale, bae FALSE
+
+def oneEdit(s1, s2):
+  if s1 == s2:
+    return True
+
+  return False
+
+
+print(oneEdit("pale","ple"))
+print(oneEdit("pales","pale"))
+print(oneEdit("pale","bale"))
+print(oneEdit("pale","bae"))
--- Problems/ch01-arrays-strings/p06_string_compression.py
+++ Problems/ch01-arrays-strings/p06_string_compression.py
@@ -2,3 +2,34 @@
 # for example, 'aabccccaaa' would become 'a2b1c4a3'
 # If the compressed string would not become smaller than the original string, reuturn the original string
 #  Can assume that string has only uuppercase and lowercase letters
+
+#O(n) time complexity and linear O(n) space complexity
+def compressString(string):
+  compressed = []
+  counter = 0
+  for i in range(len(string)):
+    if i != 0 and string[i] != string[i-1]:
+      compressed.append(string[i-1] + str(counter))
+      counter = 0
+    counter += 1
+
+  if counter:
+    compressed.append(string[-1] + str(counter))
+
+  return min(string, "".join(compressed), key=len)
+
+
+print(compressString("aabcccccaaa"))
+print(compressString("abcdef"))
+print(compressString("aabb"))
+print(compressString("aaa"))
+print(compressString("aa"))
+print(compressString("a"))
+print(compressString(""))
+
+#  ("aabcccccaaa", "a2b1c5a3"),
+#         ("abcdef", "abcdef"),
+#         ("aabb", "aabb"),
+#         ("aaa", "a3"),
+#         ("a", "a"),
+#         ("", ""),
