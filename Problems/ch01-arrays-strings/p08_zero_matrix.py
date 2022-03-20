bd7a517d7089b9e325ccd28bdc2bc1f5a11e41b3
alexsmaldone <alexsmaldone@gmail.com>
2022-03-20 17:23:54 -0400

    zero matrix setup

--- Problems/ch01-arrays-strings/p08_zero_matrix.py
+++ Problems/ch01-arrays-strings/p08_zero_matrix.py
@@ -1,1 +1,27 @@
 # Write an algorithm such that if an element in an M x N matrix is zero, it's entire row and column are set to 0
+
+
+
+
+
+
+
+def zero_matrix(matrix):
+  cols = len(matrix)
+  rows = len(matrix[0])
+
+  curr_col = 0
+  curr_row = 0
+
+  for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+      pass
+
+
+matrix1 = [
+ [1,2,3],
+ [4,0,6],
+ [7,8,9]
+]
+
+print(zero_matrix(matrix1))
