985d0d8ef60224bece3d4c37faa1ab64105fb485
alexsmaldone <alexsmaldone@gmail.com>
2022-03-20 13:34:05 -0400

    setup fro matrix rotation problem

--- Problems/ch01-arrays-strings/p07_rotate_matrix.py
+++ Problems/ch01-arrays-strings/p07_rotate_matrix.py
@@ -1,3 +1,29 @@
 # given an image represented by an N x N matrix, where each pixel in the image is represented by an integer,
 # write a method to rotate this matrix by 90degrees
 #  Can you do this in place?
+
+# [[1,2,3],
+#  [4,5,6],
+#  [7,8,9]
+# ]
+
+# BECOMES
+
+# [[7,4,1],
+#  [8,5,2],
+#  [9,6,3]
+# ]
+
+def rotate_matrix(mx):
+  pass
+
+
+
+
+matrix = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+
+print(rotate_matrix())
