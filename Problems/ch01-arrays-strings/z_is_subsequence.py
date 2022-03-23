3277ceb7241a2ea2090a7e7e321e56eeb3446623
alexsmaldone <alexsmaldone@gmail.com>
2022-03-23 13:32:39 -0400

    isvalidsubsequence problem

---
+++ Problems/ch01-arrays-strings/z_is_subsequence.py
@@ -0,0 +1,24 @@
+
+
+# O(n)T and O(1)S
+def isValidSubsequence(array, sequence):
+  length = len(sequence)
+  counter = 0
+  seq_index = 0
+
+  i = 0
+  while i < len(array):
+    if seq_index >= length:
+      break
+    if array[i] == sequence[seq_index]:
+      counter += 1
+      seq_index += 1
+    i += 1
+
+  return counter == length
+
+arr = [5, 1, 22, 25, 6, -1, 8, 10]
+seq = [1, 6, -1, 10]
+
+
+print(isValidSubsequence(arr, seq))
