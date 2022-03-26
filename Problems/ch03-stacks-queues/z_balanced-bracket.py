81219ad389a13579b96072f34435fa2b7b98c731
alexsmaldone <alexsmaldone@gmail.com>
2022-03-26 17:12:27 -0400

    balanced brakcet stack

--- Problems/ch03-stacks-queues/z_balanced-bracket.py
+++ Problems/ch03-stacks-queues/z_balanced-bracket.py
@@ -1,5 +1,22 @@
 def balancedBrackets(string):
-  pass
+
+  openingBrackets = "({["
+  closingBrackets = ")}]"
+  matchingBrackets = {")": "(", "}": "{", "]": "[" }
+  stack = []
+
+  for char in string:
+    if char in openingBrackets:
+      stack.append(char)
+    elif char in closingBrackets:
+      if len(stack) == 0:
+        return False
+      if stack[-1] == matchingBrackets[char]:
+        stack.pop()
+      else:
+        return False
+    print(stack)
+  return len(stack) == 0
 
 test = "([])(){}(())()()"
 
